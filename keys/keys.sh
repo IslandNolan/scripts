f1afcce7baf9897ec10b8164f4723414db245126
IslandNolan <48862216+IslandNolan@users.noreply.github.com>
2023-09-24 21:22:07 -0400

    Update keys.sh

--- keys/keys.sh
+++ keys/keys.sh
@@ -2,25 +2,19 @@
 echo "@Author: Nolan B - 06-2022"
 
 read -p "Enter your Github Email Address: " ghemail
-read -p "
-Press [Enter] To generate an SSH Key--
-" lolemptyvariable
-
+read -n 1 -s -r -p "[Press any key to continue]"
 
 echo "Generating SSH Key for $hostname"
 ssh-keygen -t rsa -b 4096 -C "$ghemail"
 cat $HOME/.ssh/id_rsa.pub
 
-echo "
-Import the above key into Github as an SSH Key
-"
-read -p "Press [Enter] to generate a GPG Key--
-Ensure that you use your Github Email address: $ghemail
-" lolemptyvariable
-
-
+echo "Import the above key into Github as an SSH Key"
+echo "Ensure that you have used the right Github Email, else this will not work. Current E-mail: $ghemail"
+echo "When prompted, use RSA, 4096 Length, Key does not expire, and your github username."
+read -n 1 -s -r -p "[Press any key to continue]"
 
 echo "Generating GPG Key for $hostname"
+
 gpg --full-generate-key
 #prints gpg key.
 gpg --armor --export $ghemail
